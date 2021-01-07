b751d10a4535d50fb068e832d07b3d9243c8b4b0
Logan Thirion <thirionlogan@gmail.com>
2021-01-07 16:42:28 -0600

    convert tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { mount } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  let component;
+  beforeEach(() => {
+    component = mount(<App />);
+  });
+  it('should render the app', () => {
+    expect(component.exists('div')).toBe(true);
+  });
 });
